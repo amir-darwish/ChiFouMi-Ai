b8a9b542fb146adf1d5e625d4d4c0c942d7203eb
Amir <mohamed.amir.darwish@gmail.com>
2025-02-21 16:35:35 +0100

    ajout methodes edite

---
+++ PRO402-2/App/App/Class/Rond.cs
@@ -0,0 +1,19 @@
+
+class Rond
+{
+    private double _Diametre;
+
+    public Rond(double diametre)
+    {
+        _Diametre = diametre;
+    }
+    
+    public void Edite(double diametre)
+    {
+        _Diametre = diametre;
+    }
+    public double getDiametre()
+    {
+        return _Diametre;
+    }
+}
